e7f827bbb36f1c6fee4777db091f97df6edf53a4
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2019-12-31 14:35:20 +0000

    Update Solidity Tests, Proxy.sol

--- test/Proxy.sol
+++ test/Proxy.sol
@@ -27,6 +27,14 @@ contract Proxy {
 		{
 			return eventTickets;
 		}
+		/// @notice verify owner
+		function isOwner()
+			public
+			returns (bool)
+		{
+			(bool success, ) = address(eventTickets).call(abi.encodeWithSignature("isOwner()"));
+			return success;
+		}
 
 		/// @notice buy tickets
 		/// @param tickets number of tickets to buy
