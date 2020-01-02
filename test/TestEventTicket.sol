98723a16b6577a505cf6a69c151375d65729c8a0
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-01-02 20:07:21 +0000

    Update Solidity Tests

--- test/TestEventTicket.sol
+++ test/TestEventTicket.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.5.0;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/EventTickets.sol";
-import "../contracts/EventTicketsV2.sol";
 import "./Proxy.sol";
 
 contract TestEventTicket {
